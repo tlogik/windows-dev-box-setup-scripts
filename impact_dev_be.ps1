cc0b57c0db75112ff377e37114542da2a0c3f194
Thomas Blitz <5293103+tlogik@users.noreply.github.com>
2020-05-20 12:02:04 +0200

    outcomment things we already ran

--- impact_dev_be.ps1
+++ impact_dev_be.ps1
@@ -22,12 +22,12 @@ function executeScript {
 }
 
 #--- Setting up Windows ---
-executeScript "SystemConfiguration.ps1";
-executeScript "FileExplorerSettings.ps1";
-executeScript "RemoveDefaultApps.ps1";
-executeScript "CommonDevTools.ps1";
-executeScript "Browsers.ps1";
-executeScript "NetDeveloperCommonTools.ps1";
+#executeScript "SystemConfiguration.ps1";
+#executeScript "FileExplorerSettings.ps1";
+#executeScript "RemoveDefaultApps.ps1";
+#executeScript "CommonDevTools.ps1";
+#executeScript "Browsers.ps1";
+#executeScript "NetDeveloperCommonTools.ps1";
 
 executeScript "WSL.ps1";
 RefreshEnv
